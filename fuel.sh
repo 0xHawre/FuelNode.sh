f695491a6edd18a253d472a58b7367472f8f95c4
0xHawre <0xharway@gmail.com>
2024-07-07 03:28:05 +0330

    fixing some bugs

--- fuel.sh
+++ fuel.sh
@@ -9,18 +9,32 @@ print_ascii() {
     echo " \___//_/\_\_|  |_|\__,_| \_/\_/ |_|  \___|"
     echo "                                           "
 }
-
-# Example usage:
+echo "===================================="
 print_ascii
 echo "Welcome to the Fuel Core Setup Script"
 echo "===================================="
 
-
+# Update package lists and upgrade all installed packages
 sudo apt-get update -y && sudo apt-get upgrade -y
+echo "install necessary tools........"
 
-echo "install necessary tools"
 
+# install git 
+check_git(){
+  if command -v git &> /dev/null; then
+    git_version=$(git --version | head -n 1)
+    echo "git is installed: $git_version"
+    else "git is not installed"
+      install_git
+  fi
+}
+install_git() {
+  echo "installing git......."
+  sudo apt-get install git 
+}
+check_git
 
+# install wget 
 check_wget() {
   if command -v wget &> /dev/null; then
     wget_version=$(wget --version | head -n 1 )
@@ -34,10 +48,9 @@ install_wget() {
   echo "installing wget..."
   sudo  apt-get install wget -y
 }
-
 check_wget
 
-
+# install curl 
 check_curl() {
   if command -v curl &> /dev/null; then 
     curl_version=$(curl --version | head -n 1 )
@@ -46,16 +59,13 @@ check_curl() {
     install_curl 
   fi 
 }
-
 install_curl() {
   echo "installing curl..."
   sudo apt-get install curl -y
 }
-
 check_curl
 
-
-
+# install rust 
 check_rustc() {
   if command -v rustc &> /dev/null; then
     rustc_version=$(rustc --version | head -n 1)
@@ -64,21 +74,33 @@ check_rustc() {
     echo "rustc is not installed."
     install_rustc
   fi
-
 }
-
 install_rustc() {
   echo "installing rusrc...."
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs/ | sh -s -- -y 
 }
-
 check_rustc
+source $HOME/.cargo/env # add rust to your PATH 
 
-apt-get install git 
+# install tmux 
+check_tmux() {
+  if command -v tmux &> /dev/null; then
+    tmux_version=$(tmux --version | head -n 1)
+    echo "tmux version is ${tmux_version}"
+  else
+    "tmux is not installes"
+    install_tmux 
+  fi 
+}
+install_tmux() {
+  echo "installing tmux....."
+  sudo apt-get install tmux
+}
+check_tmux
 
+# install fuel 
 curl https://install.fuel.network | sh
 source /root/.bashrc
-
 export PATH="/root/.fuelup/bin:$PATH"
 
 fuelup toolchain install latest
@@ -94,47 +116,43 @@ fuelup default testnet
 
 echo "Now we're going to generate a new Peering Key. Make sure to save them."
 fuel-core-keygen new --key-type peering >> secret.txt
-
+cat secret.txt 
 echo "Your keys have been saved to secret.txt."
 echo "Please save them securely and press Enter after you have done so."
+read -p "If you saved you're secret key Press Enter to continue..." enter_key
 
-# Wait for the user to press Enter
-read -p "Press Enter to continue..." enter_key
-
-# Continue with the rest of your script here
-# For example:
-echo "Continuing with the rest of the story..."
-
-apt-get install tmux
 start_fuel_core() {
-    echo "Starting tmux session..."
-    tmux new-session -d -s fuelu
+  read -p "Enter the node name: " nodeName
+  read -p "Enter your secret key: " sec 
+  read -p "Enter your ETH_SEPOLIA RPC: " RPC
 
-    read -p "Enter the node name: " nodeName
-    read -p "Enter your secret key: " secretK
-    read -p "Enter your ETH_SEPOLIA RPC: " RPC
+  echo "Running fuel-core in tmux session 'fuelu'..."
 
-echo "Running fuel-core in tmux session 'fuelu'..."
-    tmux send-keys -t fuelu 
-     "fuel-core run \
-        --service-name=$nodeName  \
-        --keypair $secretK \
-        --relayer $RPC  \
-        --ip=0.0.0.0 --port=5333 --peering-port=40453 \
-        --db-path=~/.fuel-sepolia-testnet \
-        --snapshot /root/.forc/git/checkouts/std-9be0d6062747ea7/2f0392ee35a1e4dd80bd8034962d5b4083dfb8b6/.github/workflows/local-testnode \
-        --utxo-validation --poa-instant false --enable-p2p \
-        --reserved-nodes /dns4/p2p-testnet.fuel.network/tcp/30333/p2p/16Uiu2HAmDxoChB7AheKNvCVpD4PHJwuDGn8rifMBEHmEynGHvHrf \
-        --sync-header-batch-size 100 \
-        --enable-relayer \
-        --relayer-v2-listening-contracts=0x01855B78C1f8868DE70e84507ec735983bf262dA \
-        --relayer-da-deploy-height=5827607 \
-        --relayer-log-page-size=500 \
-        --sync-block-stream-buffer-size 30
-    " Enter
-    echo "Fuel Core started in tmux session 'fuelu'."
+  # Create a new tmux session named 'fuelu' if it doesn't exist
+  tmux new-session -d -s fuelu
+
+  # Send the fuel-core command to the 'fuelu' tmux session
+  tmux send-keys -t fuelu "
+    fuel-core run \
+      --service-name=$nodeName \
+      --keypair=$sec \
+      --relayer=$RPC \
+      --ip=0.0.0.0 --port=5333 --peering-port=40453 \
+      --db-path=~/.fuel-sepolia-testnet \
+      --snapshot=/root/.forc/git/checkouts/std-9be0d6062747ea7/2f0392ee35a1e4dd80bd8034962d5b4083dfb8b6/.github/workflows/local-testnode \
+      --utxo-validation --poa-instant=false --enable-p2p \
+      --reserved-nodes=/dns4/p2p-testnet.fuel.network/tcp/30333/p2p/16Uiu2HAmDxoChB7AheKNvCVpD4PHJwuDGn8rifMBEHmEynGHvHrf \
+      --sync-header-batch-size=100 \
+      --enable-relayer \
+      --relayer-v2-listening-contracts=0x01855B78C1f8868DE70e84507ec735983bf262dA \
+      --relayer-da-deploy-height=5827607 \
+      --relayer-log-page-size=500 \
+      --sync-block-stream-buffer-size=30
+  " Enter
+
+  echo "Fuel Core started in tmux session 'fuelu'."
 }
 
+# Call the function to start fuel-core
 start_fuel_core
-
-
+exit 0 
